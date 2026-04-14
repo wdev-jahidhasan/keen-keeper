5d7c1c6fb949e6e3c73bebcecef1134fcaadb2dc
wdev-jahidhasan <wdev-jahidhasan@gmail.com>
2026-04-14 22:48:36 +0600

    active button style added

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -6,10 +6,9 @@ export default function NotFound() {
   const router = useRouter();
 
   return (
-    <div className="min-h-[70vh] flex items-center justify-center bg-base-100 px-4">
+    <div className="min-h-[50vh] flex items-center justify-center bg-base-100 px-4">
       
-      <div className="text-center max-w-md w-full p-6 rounded-2xl bg-gradient-to-br from-primary/10 via-secondary/10 to-accent/10">
-        <div className="mx-auto mb-3 w-16 h-16 rounded-full flex items-center justify-center bg-gradient-to-r from-primary to-secondary text-white text-2xl">🧭</div>
+      <div className="text-center max-w-7xl w-full p-6 rounded-2xl bg-gradient-to-br from-primary/10 via-secondary/10 to-accent/10">
         <h1 className="text-6xl font-extrabold bg-gradient-to-r from-primary via-secondary to-accent text-transparent bg-clip-text leading-none">404</h1>
         <h2 className="mt-2 text-xl font-semibold">
           Page Not Found
@@ -17,21 +16,6 @@ export default function NotFound() {
         <p className="mt-2 text-sm opacity-70">
           The page you’re looking for doesn’t exist or may have been moved.
         </p>
-        <div className="mt-5 flex flex-col sm:flex-row gap-3 justify-center">
-          <button
-            onClick={() => router.back()}
-            className="btn btn-outline btn-primary w-full sm:w-auto"
-          >
-            ⬅ Go Back
-          </button>
-
-          <button
-            onClick={() => router.push("/")}
-            className="btn btn-outline btn-primary w-full sm:w-auto"
-          >
-            🏠 Home
-          </button>
-        </div>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
+'use client'
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { IoHomeOutline } from "react-icons/io5";
 import { RiTimeLine } from "react-icons/ri";
 import { TfiStatsUp } from "react-icons/tfi";
 const Navbar = () => {
+
+
+  const pathName = usePathname()
+  console.log(pathName);
+
   return (
     <div className="navbar bg-base-100 shadow-sm px-5 md:px-8 lg:px-16 sticky top-0 z-50">
       <div className="navbar-start">
@@ -14,18 +21,30 @@ const Navbar = () => {
           <ul
             tabIndex="-1"
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-32 p-2 shadow">
-            <li><Link href="/" className="text-lg"><IoHomeOutline /> Home</Link></li>
-          <li><Link href="/timeline" className="text-lg"><RiTimeLine /> Timeline</Link></li>
-          <li><Link href="/stats" className="text-lg"><TfiStatsUp /> Stats</Link></li>
+            <li>
+              <Link href="/" className={`text-lg ${pathName === '/' ? 'bg-gray-300' : ''}`}><IoHomeOutline /> Home</Link>
+            </li>
+            <li>
+              <Link href="/timeline" className={`text-lg ${pathName === '/timeline' ? 'bg-gray-300' : ''}`}><RiTimeLine /> Timeline</Link>
+            </li>
+            <li>
+              <Link href="/stats" className={`text-lg ${pathName === '/stats' ? 'bg-gray-300' : ''}`}><TfiStatsUp /> Stats</Link>
+            </li>
           </ul>
         </div>
         <Image width={150} height={50} src="/logo.png" alt="logo"></Image>
       </div>
       <div className="navbar-end hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <li><Link href="/" className="text-lg"><IoHomeOutline /> Home</Link></li>
-          <li><Link href="/timeline" className="text-lg"><RiTimeLine /> Timeline</Link></li>
-          <li><Link href="/stats" className="text-lg"><TfiStatsUp /> Stats</Link></li>
+          <li>
+            <Link href="/" className={`text-lg ${pathName === '/' ? 'bg-[#244D3F] text-white' : ''}`}><IoHomeOutline /> Home</Link>
+          </li>
+          <li>
+            <Link href="/timeline" className={`text-lg ${pathName === '/timeline' ? 'bg-[#244D3F] text-white' : ''}`}><RiTimeLine /> Timeline</Link>
+          </li>
+          <li>
+            <Link href="/stats" className={`text-lg ${pathName === '/stats' ? 'bg-[#244D3F] text-white' : ''}`}><TfiStatsUp /> Stats</Link>
+          </li>
         </ul>
       </div>
     </div>
