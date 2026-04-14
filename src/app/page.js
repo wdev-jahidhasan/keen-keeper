b9fc72b552ea57930c769fb381995b1dfbb0d058
wdev-jahidhasan <wdev-jahidhasan@gmail.com>
2026-04-14 16:38:05 +0600

    fake data generated

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className='text-center text-5xl text-green-400'>
-      The Real Homepage
+      <h1>The Real Homepage</h1>
     </div>
   );
 }
