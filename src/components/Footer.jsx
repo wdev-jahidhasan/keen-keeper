b4b8b170f0f4e647114d8d98fac1f9d44b23f27a
wdev-jahidhasan <wdev-jahidhasan@gmail.com>
2026-04-14 06:37:09 +0600

    footer section to be continued

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,38 @@
+import Image from 'next/image';
 import React from 'react';
+import { FaInstagram } from "react-icons/fa";
+import { IoLogoFacebook } from "react-icons/io";
+import { FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
-      <nav>
-        <h6 className="footer-title">Services</h6>
-        <a className="link link-hover">Branding</a>
-        <a className="link link-hover">Design</a>
-        <a className="link link-hover">Marketing</a>
-        <a className="link link-hover">Advertisement</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Company</h6>
-        <a className="link link-hover">About us</a>
-        <a className="link link-hover">Contact</a>
-        <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
-      </nav>
-      <nav>
-        <h6 className="footer-title">Legal</h6>
-        <a className="link link-hover">Terms of use</a>
-        <a className="link link-hover">Privacy policy</a>
-        <a className="link link-hover">Cookie policy</a>
-      </nav>
-    </footer>
+    <footer className="footer footer-horizontal footer-center bg-[#244D3F]  p-10 text-white">
+
+      <Image width="250" height="50" src="/logo-xl.png" alt="footer logo"></Image>
+
+  <aside>
+    
+    <p>
+      Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.</p>
+
+    <p>Social Links</p>
+    <div>
+      
+    </div>
+    
+  </aside>
+  <nav>
+    <div className="grid grid-flow-col gap-4">
+     <FaInstagram className="" />
+     <IoLogoFacebook />
+     <FaTwitter />
+    </div>
+  </nav>
+
+  <div>
+    <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+  </div>
+</footer>
   );
 };
 
